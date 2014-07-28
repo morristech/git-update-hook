85087a298ae380e3956001cc802d4d3c473c1132
mattes <matthias.kadenbach@gmail.com>
2014-07-28 15:58:21 +0200

    notes

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+// refname="$1"
+// oldrev="$2"
+// newrev="$3"
+
+// get files for newrev
+// find yaml files
+// parse yaml files
+
+// exit code = 0 if ok
+// exit code > 0 if failed
+// ... print messages to stdout
